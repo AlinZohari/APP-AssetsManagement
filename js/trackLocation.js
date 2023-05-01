9766e2a9abd98a083cde8b562d5363c05851bfdf
AlinZohari <zohari.alin@gmail.com>
2023-05-01 15:22:49 +0100

    debugging the functions in trackLocation.js and creted the calculateDistance() function for A2 proximity alert

--- js/trackLocation.js
+++ js/trackLocation.js
@@ -21,25 +21,6 @@ let user_id;
 function trackLocation(){
 
 	if(navigator.geolocation){
-		// test to see if there is an active tracking and clear it if so that we don’t have multiple tracking going on
-		try {
-			(navigator.geolocation.clearWatch(geoLocationID));
-		} catch (e) {
-			console.log(e);
-		}
-
-		// clear any existing data from the map
-		removeTracks();
-
-		// need to tell the tracker what we will do with the coordinates – showPosition
-		// also what we will do if there is an error – errorPosition
-		// also set some parameters – e.g how often to renew, what timeout to set
-		const options = {
-			enableHighAccuracy: true,
-			maximumAge: 30000,
-			timeout: 27000
-		};
-
 		geoLocationID = navigator.geolocation.watchPosition(showPositionData, errorPosition, options);
 		}
 		else {
@@ -48,19 +29,9 @@ function trackLocation(){
 }
 
 
-function errorPosition(error) {
-	alert(error);
-}
-
-
 function showPositionData(position) {
 	// add the new point into the array
 	// the 'push' command
-
-	//remove the previous marker from map
-	if (trackLocationLayer.length >0){
-		mymap.removeLayer(trackLocationLayer[trackLocationLayer.length-1]);
-	}
 
 	document.getElementById('latitude').innerHTML = position.coords.latitude; 
 	document.getElementById('longitude').innerHTML = position.coords.longitude;
@@ -87,10 +58,9 @@ function removeTracks() {
 	// also we use -1 as arrays in javascript start counting at 0
 	for (let i=trackLocationLayer.length-1; i > -1;i--) {
 		console.log("removing point " +i + " which has coordinates " + trackLocationLayer[i].getLatLng());
-		mymap.removeLayer(trackLocationLayer[i]);}
-	
-		trackLocationLayer = []; // clear the array outside the loop
-		
+		mymap.removeLayer(trackLocationLayer[i]);
+	}
+
 }
 
 // userId (endpoint: userId) --------------------------------------------------------------
@@ -109,3 +79,31 @@ function userId(){
      });
 };
 
+// for A2 (Advanced Functionality 1) ----------------------------------------------------------------------
+// condition form pop up automaticlly proximity alert) when the user is close to ONE OF THEIR OWN asset points
+// adapted from the getDistance.js exercise in practical class of CEGE0043
+
+//code adapted from https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-in-your-web-apps.html
+function calculateDistance(lat1, lon1, lat2, lon2,unit){
+	
+	//first convert all the degree values into radians
+	//1 radian is the angle created in a circle when the arc is equal length to the radius
+	let radlat1 = Math.PI * lat1/180;
+	let radlat2 = Math.PI * lat2/180;
+	let radlon1 = Math.PI * lon1/180;
+	let radlon2 = Math.PI * lon2/180;
+
+	//find the difference in longitude and convert to radians
+	let theta = lon1-lon2;
+	let radtheta = Math.PI * theta/180;
+
+	//then calculate the distance
+	let subAngle = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	subAngle = Math.acos(subAngle);
+	subAngle = subAngle * 180/Math.PI; // convert the degree value returned by acos back to degrees from radians
+	let dist = (subAngle/360) * 2 * Math.PI * 3956; // ((subtended angle in degrees)/360) * 2 * pi * radius)
+	if (unit=="K") { dist = dist * 1.609344 ;} // convert miles to km
+	if (unit=="N") { dist = dist * 0.8684 ;} // convert miles to nautical miles
+	return dist;
+
+}
